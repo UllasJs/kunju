d95571329b76c92fa99c1ffde532d4f75b56bae9
ullas-fple <157341551+ullas-fple@users.noreply.github.com>
2024-05-25 05:26:23 +0000

    Deploying to gh-pages from @ UllasJs/kunju@d05ff223f543db2fe86485028f3d95e810e6b96c 🚀

---
+++ _nuxt/C4dr4BPr.js
@@ -0,0 +1,1 @@
+import{_ as t,v as s,t as o,x as e}from"./48hY3jkI.js";const c={},n={class:"content"},_=e("section",{class:"hero-section"},[e("div",null," update "),e("div",null," world ")],-1),a=[_];function r(d,i){return o(),s("div",n,a)}const f=t(c,[["render",r]]);export{f as default};
