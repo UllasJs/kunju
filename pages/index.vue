af77ea79431637b45d47e823d76529dae1698aec
UllasFPLE <ullasjs@fple.in>
2024-05-25 10:33:43 +0530

    init

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="content">
+        <section class="hero-section">
+            <div>
+                test1
+            </div>
+            <div>
+                test2
+            </div>
+        </section>
+    </div>
+</template>
